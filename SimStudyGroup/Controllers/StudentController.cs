825e563459eaf47b3a1542fda089213aae3452da
simph <simph@SIMDOPE>
2025-03-02 21:05:05 +0200

    Add project files.

---
+++ SimStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimStudyGroup.Models;
+
+namespace SimStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> student = new List<Student>
+            {
+                new Student
+                {
+                    StudentNo ="21223356",
+                    Name = "Sam",
+                    Surname = "Ngcobo",
+                    Email = "sam.ngcobo@gmail.com"
+
+                },
+                  new Student
+                {
+                    StudentNo ="22387876",
+                    Name = "Ben",
+                    Surname = "Jason",
+                    Email = "ben.jason@gmail.com"
+
+                },
+                   new Student
+                {
+                    StudentNo ="23253756",
+                    Name = "John",
+                    Surname = "Stones",
+                    Email = "john.stones@gmail.com"
+
+                },
+                       new Student
+                {
+                    StudentNo ="24427256",
+                    Name = "Cole",
+                    Surname = "Palmer",
+                    Email = "cole.palmer@gmail.com"
+
+                },
+                        new Student
+                {
+                    StudentNo ="23782537",
+                    Name = "Kuzivashe",
+                    Surname = "Gwiza",
+                    Email = "kuzivashe.gwiza@gmail.com"
+
+                }
+
+            };
+            return View(student);
+        }
+    }
+}
---
+++ SimStudyGroup/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace SimStudyGroup.Models
+{
+	public class Student
+	{
+        [DisplayName ("STUDENT NUMBER")]
+		public string StudentNo { get; set; }
+
+
+        [DisplayName("NAME")]
+        public string Name { get; set; }
+
+
+        [DisplayName("SURNAME")]
+        public string Surname { get; set; }
+
+
+        [DisplayName("EMAIL")]
+        public string Email { get; set; }
+
+
+    }
+}
